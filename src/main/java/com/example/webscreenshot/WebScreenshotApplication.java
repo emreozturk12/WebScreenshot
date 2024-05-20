006f162e8cf401ce884bc4d9e257d2b5b2728b9e
Emre Öztürk <eozturk5258@gmail.com>
2024-05-20 18:20:55 +0300

    first commit

---
+++ src/main/java/com/example/webscreenshot/WebScreenshotApplication.java
@@ -0,0 +1,13 @@
+package com.example.webscreenshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScreenshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScreenshotApplication.class, args);
+	}
+
+}
